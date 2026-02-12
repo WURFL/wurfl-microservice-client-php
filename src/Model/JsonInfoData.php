9277537b8b50757d96a3a57340d125eba991467f
mbellomi <massimiliano.bellomi@gmail.com>
2026-02-12 13:31:58 +0100

    Adds ReturnTypeWillChange to jsonSerialize() methods for PHP 8.2+ compatibility

--- src/Model/JsonInfoData.php
+++ src/Model/JsonInfoData.php
@@ -86,6 +86,8 @@ class JsonInfoData implements \JsonSerializable
         return $this->data['ltime'];
     }
 
+    // Suppress return type deprecation to maintain PHP 7.4 compatibility
+    #[\ReturnTypeWillChange]
     public function jsonSerialize()
     {
         return $this->data;
